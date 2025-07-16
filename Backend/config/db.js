b944a05ccf20db658c396f455d8ff7f77ead03a3
Anu8084 <anuragbabaojha@gmail.com>
2025-07-16 15:37:42 +0530

    db.js has been modified

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,26 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log('MongoDB connected');
+//   } catch (error) {
+//     console.error('MongoDB connection failed:', error.message);
+//     process.exit(1); // Stop server if DB fails
+//   }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
@@ -14,3 +29,4 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
